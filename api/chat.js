49a29657aed17e8007703cb530872f7fce75678f
Rizka <uziyraf@gmail.com>
2026-01-16 15:29:15 +0700

    coba mengubungkan backend

--- api/chat.js
+++ api/chat.js
@@ -1,39 +1,39 @@
-const dialogflow = require("@google-cloud/dialogflow");
-const { v4: uuidv4 } = require("uuid");
+// const dialogflow = require("@google-cloud/dialogflow");
+// const { v4: uuidv4 } = require("uuid");
 
-const sessionClient = new dialogflow.SessionsClient({
-  credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS)
-});
+// const sessionClient = new dialogflow.SessionsClient({
+//   credentials: JSON.parse(process.env.GOOGLE_CREDENTIALS)
+// });
 
-module.exports = async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
+// module.exports = async (req, res) => {
+//   if (req.method !== "POST") {
+//     return res.status(405).json({ error: "Method not allowed" });
+//   }
 
-  const { message } = req.body;
-  try {
-    const response = await fetch("/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ message })
-    });
+//   const { message } = req.body;
+//   try {
+//     const response = await fetch("/chat", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify({ message })
+//     });
 
-    const data = await response.json();
+//     const data = await response.json();
 
-    chatBox.innerHTML += `
-    <div class="message bot">
-      ${data.reply}
-    </div>
-  `;
+//     chatBox.innerHTML += `
+//     <div class="message bot">
+//       ${data.reply}
+//     </div>
+//   `;
 
-  } catch (error) {
-    chatBox.innerHTML += `
-    <div class="message bot">
-      Terjadi kesalahan server
-    </div>
-  `;
-  }
+//   } catch (error) {
+//     chatBox.innerHTML += `
+//     <div class="message bot">
+//       Terjadi kesalahan server
+//     </div>
+//   `;
+//   }
 
-};
+// };
